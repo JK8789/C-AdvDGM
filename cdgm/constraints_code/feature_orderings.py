309b7bc563151b017fa258881aee4d74825f7e6f
Salijona Dyrmishi <sdyrmishi@iris-055.iris-cluster>
2024-09-18 17:14:13 +0200

    Use solely random ordering

--- cdgm/constraints_code/feature_orderings.py
+++ cdgm/constraints_code/feature_orderings.py
@@ -18,22 +18,12 @@ def set_random_ordering(ordering: List[Variable], to_be_frozen_ids:[]):
 
 
 def set_ordering(use_case, ordering: List[Variable], label_ordering_choice: str, model_type: str, data_partition='test'):
-    json_filename = f'cdgm/feature_ordering/feature_orderings.json'
-    with open(json_filename, "r") as f:
-        data = json.load(f)
 
     if label_ordering_choice == 'random':
         if use_case == "heloc":
             ordering = set_random_ordering(ordering, [1,12])
         else:
             ordering = set_random_ordering(ordering, [])
-
-    elif label_ordering_choice == 'causal':
-        ordering = data["feature_orderings"][use_case]['general'][label_ordering_choice]['train']
-        ordering = list(map(lambda x: Variable(x), ordering.split()))
-    else:
-        ordering = data["feature_orderings"][use_case][model_type][label_ordering_choice][data_partition]
-        ordering = list(map(lambda x: Variable(x), ordering.split()))
 
     readable_ordering = [e.readable() for e in ordering]
     print(f'Using *{label_ordering_choice}* feature ordering:\n', readable_ordering, 'len:', len(readable_ordering))
